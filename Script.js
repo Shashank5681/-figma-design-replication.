e66f51257722b7675f8dc067d6c0734e8ea76689
Shashank5681 <90023947+Shashank5681@users.noreply.github.com>
2024-06-29 11:44:36 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,6 @@
+// Add Font Awesome icons
+const statIcons = document.querySelectorAll('.stat-icon');
+statIcons.forEach(icon => {
+    icon.classList.add('fas'); // Example: 'fas fa-globe'
+    icon.classList.add('fa-globe'); // Replace with your desired icon class
+});
